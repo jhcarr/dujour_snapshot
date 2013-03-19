3ee321102899bc4455170b5b8eed2e6a69945850
Michael Stahnke <stahnma@puppetlabs.com>
2013-03-19 20:18:36 +0000

    PE Version is now at 2.7.2
    This is already updated on the live configuration running on the updates
    box. This is just a housekeeping pull request.

--- config.clj
+++ config.clj
@@ -6,12 +6,12 @@
  :latest-version
  {"puppetdb" {:version "1.0.1"
               :link    "https://github.com/puppetlabs/puppetdb/blob/1.0.2/CHANGELOG.md"}
-  "pe-master" {:version "2.7.0"
-              :message "Version 2.7.0 of Puppet Enterprise is available"
+  "pe-master" {:version "2.7.2"
+              :message "Version 2.7.2 of Puppet Enterprise is available"
               :product "pe-master"
               :link    "http://links.puppetlabs.com/enterpriseupgrade"}
- "pe-agent"  {:version "2.7.0"
-              :message "Version 2.7.0 available for this Puppet Enterprise agent"
+ "pe-agent"  {:version "2.7.2"
+              :message "Version 2.7.2 available for this Puppet Enterprise agent"
               :product "pe-agent"
               :link    "http://links.puppetlabs.com/enterpriseupgrade"}}
 }
