b3531e68620afe76dbe74290f5f6b4eb0b4aab8e
Andrew Roetker <andrew.roetker@puppetlabs.com>
2013-09-05 13:49:14 -0700

    Remove obselete data from the config.clj

--- config.clj
+++ config.clj
@@ -6,17 +6,4 @@
  {:classname "org.postgresql.Driver"
   :subprotocol "postgresql"
   :subname "//localhost:5432/dujour"}
-
-
- :latest-version
- {"puppetdb" {:version "1.0.1"
-              :link    "https://github.com/puppetlabs/puppetdb/blob/1.0.2/CHANGELOG.md"}
-  "pe-master" {:version "3.0.0"
-              :message "Version 3.0.0 of Puppet Enterprise is available"
-              :product "pe-master"
-              :link    "http://links.puppetlabs.com/enterpriseupgrade"}
- "pe-agent"  {:version "3.0.0"
-              :message "Version 3.0.0 available for this Puppet Enterprise agent"
-              :product "pe-agent"
-              :link    "http://links.puppetlabs.com/enterpriseupgrade"}}
 }
